4242782c49639f9b100377e3ced2c52eaaa64a01
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2025-09-16 17:31:44 -0300

    Add files via upload

---
+++ 1.introduccion.sql
@@ -0,0 +1,16 @@
+-Las Bases de Datos (SQL = Structured Query Languaje) es un conjunto de datos pertenecientes a un mismo contexto y almacenados sistematicamente para 
+su posterior uso. En las bases de datos se guardara información la cual podra ser usada posteriormente; pero para ello deberá ser guardada como un dato
+de forma ordenada y estructurada.
+
+-Para trabajar con una Base de Datos, se debe tener instalado tanto el motor (por ejemplo PostgreSQL) como el administrador de la misma (por ejemplo
+pgAdmin).
+
+-Las Bases de Datos estan compuestas de tablas relacionadas entre si. Cada tabla contiene datos acerca de algo. Las tablas se relacionan mediante
+columnas de identificacion, cuyo objetivo es almacenar los id necesarios para, por ejemplo, identificar de forma unica a una persona para asi
+vincularla con otro dato de otra tabla.
+
+-En las Bases de Datos se pueden almacenar diferentes tipos de datos: 
+Datos de texto (nombres, apellidos)
+Datos numericos(edad, estatura) 
+Datos de fecha (dia de registro, dia de nacimiento)
+Datos booleanos (valores true o false)
