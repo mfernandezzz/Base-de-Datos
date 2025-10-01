f7ea25f0bbdcc99eb418ac6f5c6fa6059eb72eb8
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2025-10-01 12:11:52 -0300

    4.alter_table.sql

--- 4.alter_table.sql
+++ 4.alter_table.sql
@@ -6,12 +6,13 @@ ALTER TABLE nombreTabla ADD COLUMN nombreColumna tipodeDato;
 ALTER TABLE nombreTabla ADD CONSTRAINT nombre_de_la_restriccion FOREIGN KEY (nombre_columna) REFERENCES tabla(id);
 
 Ejemplo de uso de la sentencia ALTER TABLE para modificar una tabla, agregarle una columna y asignarle una restriccion.
-ALTER TABLE film ADD column director_id INT;
+ALTER TABLE film ADD COLUMN director_id INT;
 ALTER TABLE film ADD CONSTRAINT fk_film_director FOREIGN KEY(director_id) REFERENCES director(director_id);
 
 Cambiar el tipo de dato de una columna en una tabla 
 ALTER TABLE cars
-ALTER COLUMN year type varchar(4);  -si el tipo de dato era int, ahora pasara a ser varchar
+ALTER COLUMN year type VARCHAR(4);  -si el tipo de dato era int, ahora pasara a ser varchar
 
 ALTER TABLE cars
-ADD color varchar(50);
+
+ADD color VARCHAR(50);
