17cc8c35a8e6f8308f8ffdf042f3e07d4907bd68
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2025-10-01 12:19:19 -0300

    7.delete.sql

--- 7.delete.sql
+++ 7.delete.sql
@@ -1,24 +1,25 @@
 La sentencia DELETE permite borrar informacion de las tablas.
 
-Eliminar todas las filas de una tabla
-DELETE from ESTUDIANTES;
+Eliminar todas las filas de una tabla:
+DELETE FROM ESTUDIANTES;
 
-Eliminar una fila de una tabla si contiene los datos especificados
-DELETE from director 
-    WHERE first_name='Clint' and last_name='Almodóvar';  -se elimina una fila de una tabla si contiene los datos especificados
+Eliminar una fila de una tabla si contiene los datos especificados:
+DELETE FROM director 
+WHERE first_name='Clint' and last_name='Almodóvar';
 
-Eliminar una fila de una tabla en base a su id
+Eliminar una fila de una tabla en base a su id:
 DELETE FROM director
-    WHERE director_id = 5;
+WHERE director_id = 5;
 
-Eliminar una columna de una tabla
+Eliminar una columna de una tabla:
 ALTER TABLE cars
-DROP column year;
+DROP COLUMN year;
 
-Eliminar todos los resultados si una columna contiene el dato especificado
+Eliminar todos los resultados si una columna contiene el dato especificado:
 DELETE FROM cars
 WHERE brand = 'volvo';
 
-Asi como una tabla puede ser creada tambien puede ser eliminada.
+Asi como una tabla puede ser creada tambien puede ser eliminada:
 DROP TABLE videoujuegos; 
+
 
