ba749a952c25e582fbf78dd8456b5eb031db6666
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2025-10-03 20:12:24 -0300

    2.tipos_de_datos.sql

--- 2.tipos_de_datos.sql
+++ 2.tipos_de_datos.sql
@@ -1,3 +1,19 @@
+Las Bases de Datos (SQL: Structured Query Languaje) es un conjunto de datos pertenecientes a un mismo contexto y almacenados sistematicamente para su
+posterior uso. En las bases de datos se guardara información la cual podra ser usada posteriormente; pero para ello deberá ser guardada como un dato de forma
+ordenada y estructurada.
+  
+Para trabajar con una Base de Datos, se debe tener instalado tanto el motor (PostgreSQL) como el administrador de la misma (pgAdmin).
+  
+Las Bases de Datos estan compuestas de tablas relacionadas entre si. Cada tabla contiene datos acerca de algo. Las tablas se relacionan mediante columnas de
+identificacion, cuyo objetivo es almacenar los id necesarios para, por ejemplo, identificar de forma unica a una persona para asi vincularla con otro dato
+de otra tabla.
+
+En las Bases de Datos se pueden almacenar diferentes tipos de datos: 
+Datos de texto (nombres, apellidos)
+Datos numericos(edad, estatura) 
+Datos de fecha (dia de registro, dia de nacimiento)
+Datos booleanos (valores true o false)
+
 VARCHAR: significa 'varying character' y permite almacenar un dato de texto determinando su largo de caracteres. 
 nombre VARCHAR(20)  -el nombre del dato, la sentencia y la cantidad de caracteres.
 email VARCHAR(30)
