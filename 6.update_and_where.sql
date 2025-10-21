cc3c18a0cd6d965876e89d4e1cda188afb337f90
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2025-10-21 18:07:44 -0300

    Update 6.update_and_where.sql

--- 6.update_and_where.sql
+++ 6.update_and_where.sql
@@ -1,6 +1,6 @@
-La propiedad UPDATE puede ser utilizado tanto para modificar algún dato como agregar informacion nueva a una tabla, especificando su nombre. Con SET 
-se establece el nombre de la columna y su respectivo valor. Con WHERE se puede indicar el id para identificar la fila correcta donde se modificara 
-dicho valor.
+La propiedad UPDATE puede ser utilizado tanto para modificar algún dato/s como agregar informacion nueva a una tabla. Con SET se establece el nombre de la 
+columna y su respectivo valor. Con WHERE se puede indicar (por ejemplo) el id para identificar la fila correcta donde se modificara dicho valor.
+Se puede modificar el valor a mas de una columna. Si se omite la clausula WHERE, todas las filas de la tabla veran cambiado el dato introducido.
 
 UPDATE estudiantes
 SET color_favorito='naranja'
@@ -13,27 +13,24 @@ WHERE id=2;
 UPDATE estudiantes
 SET color_favorito='naranja'
 WHERE nombre='Punku' AND apellido='Nose'; 
-Se puede utilizar el operador condicional AND para identificar la fila donde se va a modificar un dato en base al valor de dos columnas. 
+--uso del operador condicional AND para identificar la fila donde se va a modificar un dato en base al valor de dos columnas. 
 
 UPDATE estudiantes
 SET color_favorito='naranja'
 WHERE nombre='Punku' OR apellido='Diaz';  
-En este caso se utiliza el operador condicional OR para indicar que se quiere modificar el valor de una columna en aquellas filas donde la columna
-nombre y apellido tengan los valores correspondientes. Esto permite cambiar el dato de una columna para muchas filas de datos.
+--se utiliza el operador condicional OR para indicar que se quiere modificar el valor de una columna en aquellas filas donde la columna nombre y apellido
+--tengan los valores correspondientes. Esto permite cambiar el dato de una columna para muchas filas de datos.
 
 UPDATE store
 SET address_id = '6'
 WHERE store_id = '1';
 
-Se puede modificar el valor a mas de una columna.
-Si se omite la clausula WHERE, todas las filas de la tabla veran cambiado el dato introducido.
-
-Modificar la tabla cars el valor de la columna color por 'blue' y año por 1970 donde la columna brand sea 'Toyota'.
+Modificar la tabla cars el valor de la columna color por 'blue' y año por 1970 donde la columna brand sea 'Toyota':
 UPDATE cars
 SET color = 'blue', year = 1970
 WHERE brand = 'Toyota';
 
-Asignar el modelo bronco a todos los elementos que tengan la marca 'Toyota'
+Asignar el modelo bronco a todos los elementos que tengan la marca 'Toyota':
 UPDATE cars
 SET model = 'Bronco'
 WHERE brand = 'Toyota';
