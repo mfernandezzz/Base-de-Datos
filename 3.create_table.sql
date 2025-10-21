8c82b5251c088c4a3df7006707dd3707f120ddc3
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2025-10-21 17:36:02 -0300

    Update 3.create_table.sql

--- 3.create_table.sql
+++ 3.create_table.sql
@@ -1,17 +1,14 @@
-Las consultas permiten extraer informacion y manipular datos; agregando, eliminando o cambiando lo que queramos.
-Nunca se debe poner el mismo nombre a dos bases de datos o a dos tablas.
-
-CREATE TABLE estudiantes(
+CREATE TABLE estudiantes (
     id SERIAL PRIMARY KEY,
-    nombre VARCHAR(15) NOT NULL,
-    apellido VARCHAR(20) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
     edad INT NOT NULL
 );
 
-CREATE TABLE director(
-    director_id SERIAL PRiMARY KEY,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL 
+CREATE TABLE director (
+    director_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL 
 );
 
 CREATE TABLE videojuegos(
@@ -23,4 +20,3 @@ CREATE TABLE videojuegos(
 	lenguaje VARCHAR(15) NOT NULL,
 	Última_actualización DATE NOT NULL 
 );
-
