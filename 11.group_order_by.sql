174bce31b6ea4e839185c9293d65930ee302964d
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2025-10-25 18:09:09 -0300

    Update 11.group_order_by.sql

--- 11.group_order_by.sql
+++ 11.group_order_by.sql
@@ -1,5 +1,4 @@
-Las funciones de agregacion son utilizadas para obtener, agrupar y ordenar datos de una tabla o tablas conforme a la forma en la que se desee 
-visualizar los datos.
+Las funciones de agregacion son utilizadas para obtener, agrupar y ordenar datos de una tabla o tablas conforme a la forma en la que se desee visualizar los datos.
 Sentencia GROUP BY: se utiliza para agrupar los datos en base a la columna especificada.
 Sentencia ORDER BY: se utiliza para ordenar los datos consultados.
 La estructura de una query utilizando ambas funciones es la siguiente:
@@ -9,17 +8,17 @@ WHERE ___
 GROUP BY ___ 
 ORDER BY ___ ;
 
-Alias: funcion para cambiar el nombre tanto de columnas como de tablas. Con el uso de los alias se simplifica la tarea a la hora de trabajar con muchas
-tablas o columnas en consultas mas complejas.
-SELECT nota, count(*) AS Cantidad de alumnos
+Alias: funcion para cambiar el nombre tanto de columnas como de tablas. Con el uso de los alias se simplifica la tarea a la hora de trabajar con muchas tablas o
+columnas en consultas mas complejas.
+SELECT nota, COUNT(*) AS Cantidad de alumnos
 FROM resultados AS R  --la tabla resultados pasa a llamarse R
 GROUP BY nota; --El resultado es una tabla de dos columnas con los diferentes valores de nota y al lado la cantidad de estudiantes que obtuvieron dicha nota.
 
-Obtener una tabla con el identificador de cada actor junto con la cantidad de películas que protagonizo en orden descendente:
+Obtener una tabla con el identificador de cada actor junto con la cantidad de películas que protagonizo:
 SELECT actor_id, COUNT(film_id) AS cantidad
 FROM film_actor
 GROUP BY actor_id
-ORDER BY cantidad DESC;
+ORDER BY cantidad DESC; --se ordenan las filas en orden descendente de cantidad de peliculas
 
 Obtener una tabla con el identificador de cada cliente junto con la cantidad de alquileres que hizo:
 SELECT customer_id, COUNT(rental_id) AS alquileres
@@ -39,15 +38,8 @@ FROM estudiantes
 GROUP BY edad;
 ORDER BY promedio_nota ASC;
 
-Obtener el dato de la pelicula con la duracion de renta mas larga:
-SELECT film.title AS titulo, MAX(rental_duration)
-FROM film
-GROUP BY rental_duration
-ORDER BY rental_duration DESC;
-
 Obtener todos los datos de la tabla usuarios agrupados por el id_usuario y ordenar las filas en base al valor de la columna edad en orden descendente:
 SELECT *
 FROM usuarios
 GROUP BY id_usuario
 ORDER BY edad DESC;
-
