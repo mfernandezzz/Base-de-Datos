73a802839c54c6108c6da2f65ef4f88bb3d0888e
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2025-10-25 21:40:11 -0300

    Update proyecto.sql

--- proyecto.sql
+++ proyecto.sql
@@ -1,13 +1,4 @@
 Parte 1: Analisis
-Realizar una consulta que muestre el monto total facturado por cada local:
-SELECT store.store_id, SUM(payment.amount) AS total_amount
-FROM payment
-LEFT JOIN staff
-    ON payment.staff_id = staff.staff_id
-INNER JOIN store
-    ON staff.store_id = store.store_id
-GROUP BY store.store_id;
-
 Realizar una consulta para visualizar las ventas por mes para todo el periodo. La consulta debe tener el monto total para cada mes del periodo que se
 tenga datos. Ordenar por recaudacion de mayor a menor.
 SELECT to_char(rental.rental_date, 'YYYY-MM') AS month, sum(payment.amount) AS ganancias
@@ -18,30 +9,30 @@ WHERE rental.rental_date BETWEEN '2005-05-24' AND '2006-03-14'
 GROUP BY month
 ORDER BY ganancias DESC;
 
-Obtener una tabla con todas las categorias de peliculas, la cantidad de rentas por categoria, las ganancias por categoria y el porcentaje que
-represento lo recaudado por categoria respecto de la recaudacion total. Ordenar por ganancia de mayor a menor.
-SELECT category.name AS category, COUNT(rental.rental_id) AS rentals, SUM(payment.amount) AS ganancias, ROUND(SUM(payment.amount) * 100 / 61312.04) AS porcentaje_recaudacion
+Obtener una tabla con las categorias de peliculas, la cantidad de rentas por categoria, las ganancias por categoria y el porcentaje que represento lo
+recaudado por categoria respecto de la recaudacion total. Ordenar por ganancia de mayor a menor.
+SELECT category.name AS category, COUNT(rental.rental_id) AS rentals, SUM(payment.amount) AS ganancias, (SUM(payment.amount) * 100 / 61312.04) AS porcentaje_recaudacion
 FROM category
-LEFT JOIN film_category
+JOIN film_category
 	ON category.category_id = film_category.category_id
-LEFT JOIN inventory
+JOIN inventory
 	ON film_category.film_id = inventory.film_id
-LEFT JOIN rental
+JOIN rental
 	ON inventory.inventory_id = rental.inventory_id
-LEFT JOIN payment
+JOIN payment
 	ON rental.rental_id = payment.rental_id
 GROUP BY category
 ORDER BY ganancias DESC;
 
-Obtener una tabla con las categorias de peliculas, la cantidad de rentas por categoria y el porcentaje de rentas para cada categoria con respecto de
-la cantidad maxima de rentas. Ordenar por cantidad de rentas de mayor a menor.
+Obtener una tabla con las categorias de peliculas, la cantidad de rentas por categoria y el porcentaje de rentas para cada categoria con respecto de la
+cantidad maxima de rentas. Ordenar por cantidad de rentas de mayor a menor.
 SELECT category.name AS category, COUNT(rental.rental_id) AS rentals, (COUNT(rental.rental_id) * 100 / 16044) AS porcentaje_rentas
 FROM category
-LEFT JOIN film_category
+JOIN film_category
 	ON category.category_id = film_category.category_id
-LEFT JOIN inventory
+JOIN inventory
 	ON film_category.film_id = inventory.film_id
-LEFT JOIN rental
+JOIN rental
 	ON inventory.inventory_id = rental.inventory_id
 GROUP BY category
 ORDER BY rentals DESC;
@@ -49,13 +40,13 @@ ORDER BY rentals DESC;
 Identificar los 3 actores cuyas peliculas son las mas alquiladas en cantidad de rentas y recaudacion lograda:
 SELECT actor.first_name AS nombre, actor.last_name AS apellido, COUNT(rental.rental_id) AS cantidad_rentas, SUM(payment.amount) AS ganancias
 FROM actor
-LEFT JOIN film_actor
+JOIN film_actor
 	ON actor.actor_id = film_actor.actor_id
-LEFT JOIN inventory
+JOIN inventory
 	ON film_actor.film_id = inventory.film_id
-LEFT JOIN rental
+JOIN rental
 	ON inventory.inventory_id = rental.inventory_id
-LEFT JOIN payment
+JOIN payment
 	ON rental.rental_id = payment.rental_id
 GROUP BY nombre, apellido
 ORDER BY ganancias DESC
@@ -64,7 +55,7 @@ LIMIT 3;
 Obtener el top 6 de clientes que mas dinero gastaron:
 SELECT customer.first_name AS nombre, customer.last_name AS apellido, SUM(payment.amount) AS dinero_gastado
 FROM customer
-LEFT JOIN payment
+JOIN payment
     ON customer.customer_id = payment.customer_id
 GROUP BY nombre, apellido
 ORDER BY dinero_gastado DESC
